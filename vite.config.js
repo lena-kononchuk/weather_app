3f56244ddecc9937837a0511ec4580db48785185
lenakononchuk <lena@rockingtech.co.uk>
2024-10-02 11:47:34 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-base: '/weather_app/',  
+base: '/vite-project/',  
   resolve: {
-    // alias: {
-    //   '@': fileURLToPath(new URL('./src', import.meta.url)),
-    // },
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
 });
