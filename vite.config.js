62ef155857b92f409ef0600be5a48ed2f4e0940a
lenakononchuk <lena@rockingtech.co.uk>
2024-10-02 11:54:34 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { fileURLToPath, URL } from "url";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-base: '/vite-project/',  
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+  base: '/vite-project/',  // Убедитесь, что здесь указано название вашего репозитория на GitHub
 });
