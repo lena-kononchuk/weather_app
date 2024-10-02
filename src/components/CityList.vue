4559a0dd050dfac317b3d1f70482e3aa63f08845
lenakononchuk <lena@rockingtech.co.uk>
2024-10-02 19:31:51 +0300

    finish weather app

--- src/components/CityList.vue
+++ src/components/CityList.vue
@@ -13,49 +13,49 @@ import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import CityCard from "../components/CityCard.vue";
 
-// Создание реактивной ссылки для хранения сохраненных городов
 const savedCity = ref([]);
 
-// Асинхронная функция для получения данных о погоде
 const getCities = async () => {
-   // Проверка наличия сохраненных городов в localStorage
    if (localStorage.getItem('savedCity')) {
       savedCity.value = JSON.parse(localStorage.getItem('savedCity'));
 
       const requests = savedCity.value.map((city) => {
+         console.log('Fetching data for:', city.coords);
          return axios.get('https://api.openweathermap.org/data/2.5/weather', {
             params: {
-               lat: city.coords.lat, // Широта города
-               lon: city.coords.lng, // Долгота города
-               appid: '7288c10f83a369a476806c4d72a10cbc', // Ваш ключ API
-               units: 'metric' // Установка единиц измерения на метрические
+               lat: city.coords.lat,
+               lon: city.coords.lng,
+               appid: '7288c10f83a369a476806c4d72a10cbc',
+               units: 'metric'
             }
          });
       });
 
       try {
-         // Ожидание выполнения всех запросов параллельно
          const weatherData = await Promise.all(requests);
-         await new Promise((res) => setTimeout(res, 1000)); // Задержка в 1 секунду
+         await new Promise((res) => setTimeout(res, 1000));
 
-         // Присвоение данных о погоде каждому объекту города
          weatherData.forEach((value, index) => {
             savedCity.value[index].weather = value.data;
          });
 
          console.log('Weather data for saved cities:', savedCity.value);
       } catch (error) {
-         console.error('Error fetching weather data:', error);
+         if (error.response) {
+            console.error('Error fetching weather data:', error.response.data);
+         } else if (error.request) {
+            console.error('No response received:', error.request);
+         } else {
+            console.error('Error:', error.message);
+         }
       }
    }
 };
 
-// Вызов функции при инициализации компонента
 await getCities();
 
 const router = useRouter();
 
-// Функция для перехода к представлению города
 const goToCityView = (city) => {
    router.push({
       name: "cityView",
